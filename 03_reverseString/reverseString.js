d76d3069d5b3e49a67f6b0daf3586eb6f5460ef1
oyasumi58 <190088115+oyasumi58@users.noreply.github.com>
2025-02-27 18:12:24 +0800

    Do solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
-
+const convert = function(string) {
+    let result2 = string.split('').reverse();
+    return result2
 };
+
+let bigWord = '';
+
+const reverseString = function(string) {
+    let backWord = convert(string);
+    bigWord = '';
+    console.log(backWord);
+    for (letter of backWord) {
+    bigWord += `${letter.toString()}`;
+   }
+   return bigWord;
+}
+
+
+
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
