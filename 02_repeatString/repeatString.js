47666746cbc32aff41dabb52487a646ce4174e9d
oyasumi58 <190088115+oyasumi58@users.noreply.github.com>
2025-02-27 15:12:16 +0800

    Do solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+let result1 = '';
+const repeatString = function (string,numRepeat) {
+    result1 = '';
+    if (numRepeat <0) {
+        return "ERROR"
+    } else {
+        for (i = 0; i < numRepeat; i++) {
+         result1 += string
+        }
+        return result1
+    }
 };
+
+// couldve done result1 += string i think
+//if you didnt test with console.log i think you could've declared inside the loop
+
+console.log(repeatString('hey',3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
