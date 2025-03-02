3aa941c42b72ed196b1c5fd83cc1103f9fcb7e5d
oyasumi58 <190088115+oyasumi58@users.noreply.github.com>
2025-03-02 15:13:48 +0800

    Do solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -15,3 +15,21 @@ console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//solution
+//for better readability and cleanliness, store them
+//all in variables that are descriptive
+// const leapYears = function (year) {
+//   const isYearDivisibleByFour = year % 4 === 0;
+//   const isCentury = year % 100 === 0;
+//   const isYearDivisibleByFourHundred = year % 400 === 0;
+
+//   if (
+//     isYearDivisibleByFour &&
+//     (!isCentury || isYearDivisibleByFourHundred)
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,32 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) { 
+  if (isNaN(tempF)) {return "ERROR"}
+  let sumC = (tempF - 32) * 5 / 9
+  sumC = round(sumC);
+  return sumC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if (isNaN(tempC)) {return "ERROR"}
+  let sumF = (tempC * 9 / 5) + 32
+  sumF = round(sumF);
+  return sumF;
 };
+
+function round(number) {
+    let dpNum = Math.round(number * 10) / 10 
+    let fixed = dpNum.toFixed(1);
+    fixed = parseFloat(number.toFixed(1))
+    return fixed;
+}
+
+console.log(convertToCelsius(1));
+console.log(convertToFahrenheit(0))
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//solution
+//it was really simple
