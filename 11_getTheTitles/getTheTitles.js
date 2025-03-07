1cae35a00c4efc61f1b6f6fcf44f65c6dd324e27
oyasumi58 <190088115+oyasumi58@users.noreply.github.com>
2025-03-07 17:47:09 +0800

    Do solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(arr) {
+    return arr.map((obj) => {
+       return obj.title;
+    });
 };
+
+console.log(getTheTitles(books));
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
